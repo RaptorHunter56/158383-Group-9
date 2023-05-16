0bd15780e0480cc90dda35c503f05f1c88a1dc0b
steven brown <sb.nb.lego@Gmail.com>
2023-05-16 18:17:04 +1200

    General Updates

--- Website/Website/Website Http/Controllers/HomeController.cs
+++ Website/Website/Website Http/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Website_Http.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
